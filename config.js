1d4c3defc2780d7e01139c0527315d96326cdf2a
salvidivyang <salvisince2001@gmail.com>
2026-02-07 13:28:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dvhgpvpbj/video/upload/v1770449940/Cupid_Chokehold_MP3_nfbz3q.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dvhgpvpbj/video/upload/v1770451075/Cupid_Chokehold_Mp3_full_song_vdls7i.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
